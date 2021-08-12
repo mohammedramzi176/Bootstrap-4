eb98ffea79029f89b1636048d575ef97897ffd07
ramzi <ramzikvrafikv@gmail.com>
2021-08-12 12:28:02 +0530

    installed the grunt-contrib-imagemin module and configured imagemin task in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,8 @@ module.exports = function (grunt) {
         watch: {
             files: 'css/*.scss',
             tasks: ['sass']
-        },
+        }
+       ,
         browserSync: {
             dev: {
                 bsFiles: {
@@ -35,6 +36,46 @@ module.exports = function (grunt) {
                         baseDir: "./"
                     }
                 }
+            }
+        },
+        copy: {
+            html: {
+                files: [
+                {
+                    //for html
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]                
+            },
+            fonts: {
+                files: [
+                {
+                    //for font-awesome
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
+        },
+
+        clean: {
+            build: {
+                src: [ 'dist/']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: './',                   // Src matches are relative to this path
+                    src: ['img/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'dist/'                  // Destination path prefix
+                }]
             }
         }
     });
