a3c1a2de7f7a288879340e5fe18145782d48a8ee
ramzi <ramzikvrafikv@gmail.com>
2021-08-12 13:15:13 +0530

    gulp task added for imagemin

--- Gulpfile.js
+++ Gulpfile.js
@@ -46,3 +46,9 @@ gulp.task('copyfonts', function() {
   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
   .pipe(gulp.dest('./dist/fonts'));
 });
+// Images
+gulp.task('imagemin', function() {
+   return gulp.src('img/*.{png,jpg,gif}')
+     .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+     .pipe(gulp.dest('dist/img'));
+ });
